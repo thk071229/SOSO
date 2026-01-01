07b2bd61d80a6249df93362580339dfebebc6271
thk071229 <khthk0411@gmail.com>
2026-01-01 15:42:46 +0900

    soso 프로젝트 생성(MyBatis)

--- soso/src/main/java/com/project/soso/SosoApplication.java
+++ soso/src/main/java/com/project/soso/SosoApplication.java
@@ -1,4 +1,4 @@
-package com.kh.soso;
+package com.project.soso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- soso/src/test/java/com/project/soso/SosoApplicationTests.java
+++ soso/src/test/java/com/project/soso/SosoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kh.soso;
+package com.project.soso;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
