0bc54c8f7c8d2363cd77fc13865c423eb05413f7
thk071229 <khthk0411@gmail.com>
2026-01-02 21:25:52 +0900

    회원가입을 위한 xml Dao 생성(+비밀번호 암호화 추가)

--- soso/src/main/java/com/project/soso/dao/AccountDao.java
+++ soso/src/main/java/com/project/soso/dao/AccountDao.java
@@ -1,5 +1,8 @@
 package com.project.soso.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,8 +15,32 @@ public class AccountDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	// 회원가입
 	public void insert(AccountDto accountDto) {
 		sqlSession.insert("account.insert", accountDto);
+	}
+	// 중복검사
+	public int countByAccount(AccountDto accountDto) {
+		return sqlSession.selectOne("account.checkDuplicate", accountDto);
+	}
+	// 로그인 시간 업데이트
+	public void updateLoginTime(String accountId) {
+		sqlSession.update("account.updateLoginTime", accountId);
+	}
+	// 수정
+	public boolean update(AccountDto accountDto) {
+		return sqlSession.update("account.edit", accountDto) > 0;
+	}
+	// 아이디찾기
+	public String findAccountId(String accountContact, String accountEmail) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("accountContact", accountContact);
+		params.put("accountEmail", accountEmail);
+		return sqlSession.selectOne("account.findAccountId", params);
+	}
+	// 회원 상세조회
+	public AccountDto selectOne(String accountId) {
+		return sqlSession.selectOne("account.detail", accountId);
 	}
 	
 }
---
+++ soso/src/main/java/com/project/soso/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.project.soso.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+}
+
+
+
+
